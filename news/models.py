19cd691116efa87935e7c30c5f7aff7a05061983
Alex Conway <abconway2@gmail.com>
2017-05-06 13:28:20 -0400

    Make the display name for news items readable in admin

--- news/models.py
+++ news/models.py
@@ -1,7 +1,9 @@
-from django.db import models
 from django_extensions.db.models import TimeStampedModel, TitleSlugDescriptionModel
 
 
 class NewsItem(TitleSlugDescriptionModel, TimeStampedModel):
     class Meta:
         ordering = ('-created',)
+
+    def __str__(self):
+        return 'News: {}'.format(self.title)
