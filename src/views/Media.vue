65147522df30396c539c7f70b3b2cde8962b5d82
Alex Conway <abconway2@gmail.com>
2017-09-04 12:18:45 -0400

    style media items

--- src/views/Media.vue
+++ src/views/Media.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        Media
-        {{ mediaItems }}
+        <ul>
+            <li v-for="item in mediaItems">
+                <iframe width="550" height="330" :src="item.video_url" frameborder="0" allowfullscreen></iframe>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -24,11 +27,21 @@
 </script>
 
 <style scoped>
-    div {
+    ul {
+        list-style-type: none;
+    }
+    li {
+        text-align: center;
+        vertical-align: middle;
+        margin: 0.2em;
         width: 600px;
         background-color: rgba(220,220,220,0.8);
         border-radius: 15px;
         -webkit-border-radius: 15px;
         -moz-border-radius: 15px;
     }
+    iframe {
+        padding: 0.1em;
+    }
+
 </style>
