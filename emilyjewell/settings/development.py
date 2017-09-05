d8f6301984208e3e0762ab95da8bd6e8ad2667c8
Alex Conway <abconway2@gmail.com>
2017-09-05 15:48:25 -0400

    Setup django storages for each environment

--- emilyjewell/settings/development.py
+++ emilyjewell/settings/development.py
@@ -1,1 +1,16 @@
 from .base import *
+
+
+DEBUG_ASSETS = False
+
+if DEBUG_ASSETS:
+    INSTALLED_APPS += [
+        'storages',
+    ]
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = 'emily-jewell-media-development'
--- emilyjewell/settings/production.py
+++ emilyjewell/settings/production.py
@@ -11,3 +11,14 @@ ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 # Change 'default' database configuration with $DATABASE_URL (for Heroku).
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+
+INSTALLED_APPS += [
+    'storages',
+]
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
